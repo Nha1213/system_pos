500888c0f86d9079af9cede100771f6a2546ca1d
Nha1213 <Vothanarern@gmail.com>
2026-03-02 23:50:01 +0700

    first pro

--- src/config/sequelizeConfig.js
+++ src/config/sequelizeConfig.js
@@ -1,22 +1,22 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME || 'mysql://root:TfUanObypFpNAxTAEPMvGaqsIEOFbevG@mysql.railway.internal:3306/railway',
-  {
-    dialect: "mysql",
-  }
-);
-
 // const sequelize = new Sequelize(
-//   process.env.DB_NAME || 'backend_pos_api',
-//   process.env.DB_USER || 'root',
-//   process.env.DB_PASSWORD || '',
+//   process.env.DB_NAME || 'mysql://root:TfUanObypFpNAxTAEPMvGaqsIEOFbevG@mysql.railway.internal:3306/railway',
 //   {
-//     host: process.env.DB_HOST || 'localhost',
-//     port: process.env.DB_PORT || 3306,
-//     dialect: process.env.DB_DIALECT || 'mysql'
+//     dialect: "mysql",
 //   }
 // );
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME || 'backend_pos_api',
+  process.env.DB_USER || 'root',
+  process.env.DB_PASSWORD || 'Nha1213',
+  {
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 3306,
+    dialect: process.env.DB_DIALECT || 'mysql'
+  }
+);
 
 module.exports = sequelize;
